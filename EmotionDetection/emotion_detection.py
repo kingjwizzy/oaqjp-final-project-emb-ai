a0d23337c4b6d5e779a3d6816a80c89e97793cdd
kingjwizy <jphnsabu28@gmail.com>
2026-07-17 13:54:54 +0100

    Add emotion_detector function and package it as EmotionDetection
    Calls the Watson NLP EmotionPredict service and returns anger/disgust/
    fear/joy/sadness scores plus the dominant emotion.

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload = {"raw_document": {"text": text_to_analyze}}
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    formatted_response = json.loads(response.text)
+    emotion_scores = formatted_response['emotionPredictions'][0]['emotion']
+
+    anger_score = emotion_scores['anger']
+    disgust_score = emotion_scores['disgust']
+    fear_score = emotion_scores['fear']
+    joy_score = emotion_scores['joy']
+    sadness_score = emotion_scores['sadness']
+
+    scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+    }
+    dominant_emotion = max(scores, key=scores.get)
+    scores['dominant_emotion'] = dominant_emotion
+
+    return scores
